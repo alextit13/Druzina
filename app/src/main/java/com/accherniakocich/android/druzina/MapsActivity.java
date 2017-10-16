d7345d5044483cf3ce66589918538bacf5468679
Alexey <alexcas@mail.ru>
2017-10-16 20:39:33 +0300

    v 6

--- app/src/main/java/com/accherniakocich/android/druzina/MapsActivity.java
+++ app/src/main/java/com/accherniakocich/android/druzina/MapsActivity.java
@@ -1,20 +1,29 @@
 package com.accherniakocich.android.druzina;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+
+import com.accherniakocich.android.druzina.classes.Druzinnik;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -25,6 +34,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -33,9 +45,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int MY_PERMISSION_REQUEST_LOCATION = 1;
     private final static String LOG_TAG = "MyLogs";
 
+    private double[] coordinates;
+
     private FirebaseDatabase database;
     private DatabaseReference reference;
     private String ret = "";
+
+    private Location location;
+    private long listCount = 0;
+    private ChildEventListener mChildEventListeber;
+    private Druzinnik druzinnik;
+    private ArrayList<Druzinnik>list;
 
 
     @Override
@@ -50,27 +70,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         database = FirebaseDatabase.getInstance();
         reference = database.getReference().child("Дружинники");
         readFromFile();
+        //hereLocation();
     }
+
+    private double[] startLocated() {
+        coordinates = new double[2];
+
+        if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MapsActivity.this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            } else {
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            }
+        } else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            try {
+                //Log.d(LOG_TAG,"We are here =" + hereLocate(location.getLatitude(),location.getLongitude()));
+                Log.d(LOG_TAG," s a latit =" + location.getLatitude() + ", s a  long = " + location.getLongitude());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(MapsActivity.this, "Not found!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return coordinates;
+    } //  ЭТОТ МЕТОД НУЖНО ВЫПОЛНЯТЬ В СЕРВИСЕ И В ЦИКЛЕ, ЧТО БЫ МЫ ПОЛУЧАЛИ КООРДИНАТЫ С ЧАСТОТОЙ 1 РАЗ В 10 СЕК
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case MY_PERMISSION_REQUEST_LOCATION:
-                if (grantResults.length>0&&grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     if (ContextCompat.checkSelfPermission(MapsActivity.this,
-                            Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                            Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
                         try {
-                            Log.d(LOG_TAG,"We are here =" + location);
+                            Log.d(LOG_TAG, "We are here =" + location);
 
-                        }catch (Exception e){
+                        } catch (Exception e) {
                             e.printStackTrace();
-                            Toast.makeText(MapsActivity.this,"Not found!",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MapsActivity.this, "Not found!", Toast.LENGTH_SHORT).show();
                         }
                     }
-                }else{
+                } else {
                     Toast.makeText(this, "No permission granted!", Toast.LENGTH_SHORT).show();
                 }
         }
@@ -80,40 +130,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
 
+        list = new ArrayList<>();
+        //startLocated();
+
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listCount = dataSnapshot.getChildrenCount();
+
+                if (mChildEventListeber == null){
+                    mChildEventListeber = new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                            druzinnik = dataSnapshot.getValue(Druzinnik.class);
+                            list.add(druzinnik);
+                            if (list.size()==listCount){
+                                // тут можно прописать то если все данные выкачены с сервера
+                                for (int i = 0;i<list.size();i++){
+                                    LatLng druz = new LatLng(list.get(i).getLat(), list.get(i).getLon());
+                                    mMap.addMarker(new MarkerOptions().position(druz).title(list.get(i).getFIO()));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(druz));
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    };
+                    reference.addChildEventListener(mChildEventListeber);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         // Add a marker in Sydney and move the camera
 
-        Log.d(MainActivity.LOG_TAG,"ret = " + ret);
-        LatLng sydney = new LatLng(50,50);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        /*MarkerOptions a = new MarkerOptions()
-                .position(new LatLng(50,6));
-        Marker m = mMap.addMarker(a);
-        m.setPosition(new LatLng(50,5));*/
+        /*Log.d(MainActivity.LOG_TAG, "ret = " + ret);
+        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(sydney).title(ret));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
     }
 
 
-    private void readFromFile(){
+    private void readFromFile() {
         try {
             InputStream inputStream = this.openFileInput("name.txt");
 
-            if ( inputStream != null ) {
+            if (inputStream != null) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 String receiveString = "";
                 StringBuilder stringBuilder = new StringBuilder();
 
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                while ((receiveString = bufferedReader.readLine()) != null) {
                     stringBuilder.append(receiveString);
                 }
 
                 inputStream.close();
                 ret = stringBuilder.toString();
-                Log.d(LOG_TAG,"ret =" + ret);
+                Log.d(LOG_TAG, "ret =" + ret);
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
         } catch (IOException e) {
             Log.e("login activity", "Can not read file: " + e.toString());
